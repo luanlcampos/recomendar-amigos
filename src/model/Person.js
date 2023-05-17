94f8ce7df1425be01ff56b53b98e588cd615357e
luanlcampos <luanlcampos15@gmail.com>
2023-05-16 23:46:28 -0700

    add functions to deal with data

--- src/model/Person.js
+++ src/model/Person.js
@@ -1,4 +1,5 @@
 const { validateCPF } = require('../utils/validateCPF');
+const { readPerson, writePerson, deleteData, writeRelationship, readRecommendation, readRelationship } = require('./data');
 class Person {
     constructor(cpf, name) {
         this.cpf = cpf;
@@ -7,6 +8,31 @@ class Person {
         if (!validateCPF(this.cpf)) {
             throw new Error('CPF inválido');
         }
+    }
+
+    static getPersonByCPF(cpf) {
+        const person = readPerson(cpf);
+        return person;
+    }
+
+    save() {
+        writePerson(this);
+    }
+
+    static clearData() {
+        deleteData();
+    }
+
+    static getRelationshipsById(cpf) {
+        return readRelationship(cpf);
+    }
+
+    static addRelationship(cpf1, cpf2) {
+        writeRelationship(cpf1, cpf2);
+    }
+
+    static getRecommendations(cpf) {
+        return readRecommendation(cpf);
     }
 }
 
