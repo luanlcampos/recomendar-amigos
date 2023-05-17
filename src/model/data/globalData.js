62e6fd67defeed21e4c1e2d300e2340833fe320e
luanlcampos <luanlcampos15@gmail.com>
2023-05-16 23:47:19 -0700

    code cleanup

--- src/model/data/globalData.js
+++ src/model/data/globalData.js
@@ -1,11 +1,38 @@
 // src/data/globalData.js
 
+/**
+ * people é um map onde a key é o cpf e o valor é um objeto do tipo People
+ *  Ex: {
+ *      "11111111111": {
+ *          cpf: "11111111111",
+ *          name: "Joao"
+ *      }
+ *  } 
+ */
 let people = {};
-let relationships = [];
+
+/*
+* * relationshipsAJ é uma lista adjacente onde a chave é os cpf do usuário,
+     * e o valor é uma lista contendo todas a relações do usuário.
+     * como toda relação é bidirecional, cpf1 adiciona cpf2 e cpf2 adiciona cpf1 
+     * exemplo:
+     * Antes da relação
+     * {
+     *  "11111111111": [],
+     *  "22222222222": []
+     * }
+     * 
+     * Depois da relação
+     * 
+     * {
+     *  "11111111111": ["22222222222"],
+     *  "22222222222": ["11111111111"]
+     * }
+ *  
+ */
 let relationshipsAJ = {};
 
 module.exports = {
     people,
-    relationships,
     relationshipsAJ
 };
